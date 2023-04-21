4207069157d4bca805530fc0876a637ec9489578
Ruslan Karpov <rkarpov88@gmail.com>
2023-04-21 23:53:16 +0300

    working with localstorage and button change styles

--- src/components/card/CardTweet.js
+++ src/components/card/CardTweet.js
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import css from "./CardTweet.module.css";
 import logo from "../../image/goit_logo.png";
+import avatarDefault from "../../image/avatar.png";
 
 export const CardTweet = ({ user }) => {
-  const { id } = user;
+  const { id, tweets, followers, avatar = avatarDefault } = user;
   const [isFollowing, setIsFollowing] = useState(
     JSON.parse(localStorage.getItem(`${id}-isFollowing`)) ?? false
+  );
+  const [followersNum, setFollowersNum] = useState(
+    JSON.parse(localStorage.getItem(`${id}-followers`)) ?? followers
   );
 
   useEffect(() => {
     localStorage.setItem(`${id}-isFollowing`, JSON.stringify(isFollowing));
-  }, [isFollowing]);
+    localStorage.setItem(`${id}-followers`, JSON.stringify(followersNum));
+  }, [id, isFollowing, followersNum]);
 
   const onFollowClick = () => {
     setIsFollowing(true);
+    setFollowersNum(followersNum + 1);
   };
   const onFollowingClick = () => {
     setIsFollowing(false);
+    setFollowersNum(followersNum - 1);
   };
   return (
     <div className={css.card}>
@@ -24,16 +31,18 @@ export const CardTweet = ({ user }) => {
       <div className={css.wrapper}>
         <div className={css.picture}></div>
         <div className={css.avatar}>
-          <div className={css.avatarInner}></div>
+          <img alt="avatar" src={avatar} className={css.avatarInner}></img>
         </div>
       </div>
-      <p className={css.tweetsText}> 777 tweets</p>
-      <p className={css.followersText}>100,500 Followers</p>
+      <p className={css.tweetsText}> {tweets} tweets</p>
+      <p className={css.followersText}>
+        {followersNum.toLocaleString("en-US")} Followers
+      </p>
       <div className={css.buttonBox}>
         {isFollowing ? (
           <button
             type="button"
-            className={css.button}
+            className={`${css.button} ${css.buttonFollowing}`}
             onClick={onFollowingClick}
           >
             Following
