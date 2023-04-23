b7e5a454fcfb8a21035bc6dd9e27a1fb053800e7
Ruslan Karpov <rkarpov88@gmail.com>
2023-04-23 13:50:24 +0300

    add 404.html

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
 import { Loader } from "./components/Loader/Loader";
-
-const Home = lazy(() =>
-  import("./pages/Home/Home").then((module) => ({ default: module.Home }))
-);
-const Tweets = lazy(() =>
-  import("./pages/Tweets/Tweets").then((module) => ({
-    default: module.Tweets,
-  }))
-);
+import Home from "./pages/Home/Home";
+import { Tweets } from "./pages/Tweets/Tweets";
 
 function App() {
   return (
