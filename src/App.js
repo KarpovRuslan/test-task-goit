2103743d50c313b1169b281eb7fbe6dcadc0bd6c
Ruslan Karpov <rkarpov88@gmail.com>
2023-04-23 13:17:00 +0300

    add browserRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { Suspense } from "react";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import { Loader } from "./components/Loader/Loader";
-import Home from "./pages/Home/Home";
-import { Tweets } from "./pages/Tweets/Tweets";
+
+const Home = lazy(() =>
+  import("./pages/Home/Home").then((module) => ({ default: module.Home }))
+);
+const Tweets = lazy(() =>
+  import("./pages/Tweets/Tweets").then((module) => ({
+    default: module.Tweets,
+  }))
+);
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="tweets" element={<Tweets />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-      </Routes>
-    </Suspense>
+    <BrowserRouter basename="/test-task-goit">
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="tweets" element={<Tweets />} />
+          <Route path="*" element={<Navigate to="/" replace={true} />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task-goit">
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename="/test-task-goit"> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
