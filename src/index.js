4899c291b52ce91408da13e094e66446d07e0940
Ruslan Karpov <rkarpov88@gmail.com>
2023-04-23 12:49:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/test-task-goit">
+    <BrowserRouter basename="/test-task-goit">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
