43e0459f4dca679c72fa50261b5fb4c7be258f6f
Ruslan Karpov <rkarpov88@gmail.com>
2023-04-23 14:20:43 +0300

    start filter case

--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import { CardList } from "../../components/cardList/CardList";
 import css from "./Tweets.module.css";
 import { RiArrowGoBackFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 export const Tweets = () => {
+  const [value, setValue] = useState("all");
+  const [users, setUsers] = useState([]);
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className={css.wrapper}>
       <div className={css.buttonBox}>
         <Link to="Home" className={css.button}>
           Back <RiArrowGoBackFill className={css.icon} />
         </Link>
+      </div>
+      <div className={css.filterBlock}>
+        <label className={css.label}>
+          Filter
+          <select value={value} onChange={handleChange} className={css.list}>
+            <option value="all">all</option>
+
+            <option value="follow">follow</option>
+
+            <option value="followings">followings</option>
+          </select>
+        </label>
+        <p>Showing {value}</p>
       </div>
       <CardList />
     </div>
